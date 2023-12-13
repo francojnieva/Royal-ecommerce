b273ed3e019d32fa5ade83b5974c19b82d152465
francojnieva <franco97nieva@hotmail.com>
2023-12-13 18:00:18 -0300

    :tada: first commit. create project with Vite, add components folder.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from './components/NavBar/NavBar'
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
+
+function App() {
+
+	return (
+		<>
+			<NavBar></NavBar>
+			<ItemListContainer greeting={'Bienvenidos'}></ItemListContainer>
+		</>
+	)
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CartWidget from '../CartWidget/CartWidget'
+
+const NavBar = () => {
+	return (
+		<div>
+			<p>Royal</p>
+			<ul>
+				<li>Enlace</li>
+				<li>Enlace</li>
+				<li>Enlace</li>
+			</ul>
+			<CartWidget></CartWidget>
+		</div>
+	)
+}
+
+export default NavBar
